1097d021f45a5b391102d9bbfeb01a1e81c5054e
Alexey <anakros.symbian@gmail.com>
2013-10-20 16:43:32 +0700

    [212-216/282] about_dice_project

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -6,6 +6,14 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 #   code ...
 # end
 
+class DiceSet
+  attr_reader :values
+
+  def roll(number)
+    @values = (1..number).to_a.shuffle
+  end
+end
+
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
     dice = DiceSet.new
